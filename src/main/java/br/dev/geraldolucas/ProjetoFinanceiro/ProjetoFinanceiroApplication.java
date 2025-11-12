2acaa4074f9a2242039464d241d4406eeb2989ec
gllucass <geraldolucas10@gmail.com>
2025-11-12 18:39:22 -0300

    chore: Initial commit

---
+++ src/main/java/br/dev/geraldolucas/ProjetoFinanceiro/ProjetoFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package br.dev.geraldolucas.ProjetoFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/geraldolucas/ProjetoFinanceiro/ProjetoFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.geraldolucas.ProjetoFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
